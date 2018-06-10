2b90b6ed99f41d3bb444418d14b224715b52b4e9
Rodrigo <portnetfirebase@gmail.com>
2018-06-10 18:46:30 -0500

    primera apariencia

--- app/src/main/java/itstam/masboletos/MainActivity.java
+++ app/src/main/java/itstam/masboletos/MainActivity.java
@@ -1,13 +1,85 @@
 package itstam.masboletos;
 
+import android.app.Fragment;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView menunavegacion;
+
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static final Integer[] XMEN= {R.drawable.pulso,R.drawable.ic_inicio,R.drawable.ic_perfil};
+    private ArrayList<Integer> XMENArray = new ArrayList<Integer>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menunavegacion=(BottomNavigationView)findViewById(R.id.menu_navegacion);
+
+        menunavegacion.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragmento = null;
+                switch (item.getItemId()){
+                    case R.id.action_inicio:
+                        Toast.makeText(getApplicationContext(),"Inicio",Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.action_perfil:
+                        Toast.makeText(getApplicationContext(),"Perfil",Toast.LENGTH_LONG).show();
+                    break;
+                }
+
+                return true;
+            }
+        });
+        init();
     }
+
+    private void init() {
+        for(int i=0;i<XMEN.length;i++)
+            XMENArray.add(XMEN[i]);
+
+        mPager = (ViewPager) findViewById(R.id.carrusel);
+        mPager.setAdapter(new MyAdapter(MainActivity.this,XMENArray));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == XMEN.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 2500, 2500);
+    }
+
+
+
 }
+
